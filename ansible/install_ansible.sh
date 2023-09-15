db10220501479844885d0461e1da993bb8219b23
Radoslav <125762359+stoimenovrado@users.noreply.github.com>
2023-09-15 18:29:55 +0300

    Update install_ansible.sh

--- ansible/install_ansible.sh
+++ ansible/install_ansible.sh
@@ -1,6 +1,6 @@
 #bin $bash
 
-echo "* Install ansible ..."
+echo "* Install ansible and addons ..."
 sudo apt update
 sudo apt install software-properties-common -y
 sudo add-apt-repository --yes --update ppa:ansible/ansible
@@ -11,3 +11,9 @@ sudo apt install python3-pip -y
 sudo pip3 install --upgrade pip
 sudo ansible-galaxy collection install azure.azcollection --force
 sudo pip3 install -r /root/.ansible/collections/ansible_collections/azure/azcollection/requirements-azure.txt
+
+echo "* Apply LVM Conf playbook ..."
+mkdir /home/radoslv/ansible
+cd /home/radoslv/ansible
+wget https://raw.githubusercontent.com/stoimenovrado/k8s-and-provisioning/main/ansible/lvm-conf.yaml
+sudo ansible-playbook lvm-conf.yaml
