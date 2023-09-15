4bf746e913f399e4445c099b1d03108be01c7a30
Radoslav <125762359+stoimenovrado@users.noreply.github.com>
2023-09-15 16:46:20 +0300

    Update install_ansible.sh

--- ansible/install_ansible.sh
+++ ansible/install_ansible.sh
@@ -9,5 +9,5 @@ sudo apt install sshpass -y
 sudo apt install vim -y
 sudo apt install python3-pip -y
 sudo pip3 install --upgrade pip
-ansible-galaxy collection install azure.azcollection
+sudo ansible-galaxy collection install azure.azcollection --force
 sudo pip3 install -r /root/.ansible/collections/ansible_collections/azure/azcollection/requirements-azure.txt
